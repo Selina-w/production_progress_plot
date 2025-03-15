8b5e07a2b90672f581c041a9d87f92a2f89ff07c
Selina Wang <145884057+Selina-w@users.noreply.github.com>
2025-03-15 00:30:45 -0400

    Update production_test.py

--- production_test.py
+++ production_test.py
@@ -35,7 +35,7 @@ def load_user_data(user_id):
             return data
     return {"all_styles": []}
 
-plt.rcParams['font.sans-serif'] = [ 'Arial Unicode MS']
+plt.rcParams['font.sans-serif'] = [ 'Arial Unicode MS','PingFang HK', 'Songti SC']
 #plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['axes.unicode_minus'] = False  # Fix minus signs
 plt.rcParams['figure.dpi'] = 300
